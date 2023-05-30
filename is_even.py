a4e49f7ae1c7f265442bd3d3195f6dbf5f3b726a
semccomas <semccomas@gmail.com>
2023-05-30 13:40:14 +0200

    is_even has documentation

---
+++ is_even.py
@@ -0,0 +1,9 @@
+def is_even(x):
+	"""
+	Return True if integer is even
+	Return False if integer is not even
+	Raise TypeError if x is not an integer
+	"""
+	return
+
+assert is_even.__doc__ is not None
