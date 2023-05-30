05f03a5c409311e173cd18369d190675a54a669d
semccomas <semccomas@gmail.com>
2023-05-30 13:44:31 +0200

    is_even takes integer as input

--- is_even.py
+++ is_even.py
@@ -4,6 +4,15 @@ def is_even(x):
 	Return False if integer is not even
 	Raise TypeError if x is not an integer
 	"""
-	return
+	return True
 
 assert is_even.__doc__ is not None
+
+number = False
+try:
+	is_even('sdfserw')
+except:
+	number = True
+
+assert is_even(number)
+
